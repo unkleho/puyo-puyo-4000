000a0c2d69722ff958c81aa4e97c71f43ff964f1
Kaho Cheung <kaho@elefanttraks.com>
2022-04-24 16:52:46 +1000

    Update social card

--- pages/social-card.tsx
+++ pages/social-card.tsx
@@ -3,22 +3,22 @@ import { PuyoPuyoLogo } from '../components/PuyoPuyoLogo';
 
 const SocialCardPage: NextPage = () => {
   return (
-    <article className="m-10 flex h-[800px] w-[800px] justify-between border border-stone-500 p-10">
+    <article className="m-10 flex h-[800px] w-[800px] justify-between border border-stone-500 p-16">
       <div className="mr-8 flex flex-col justify-between">
         <PuyoPuyoLogo fontSize="5em" />
 
         <div className="mt-auto mb-4 flex">
-          <div className="flex-1 border-b border-stone-500"></div>
+          <div className="flex-1 border-b-4 border-stone-500"></div>
           <div
-            className="-ml-[0.25em] mb-[0.5em] w-[1.421em] -rotate-45 border-b border-stone-500"
+            className="-ml-[0.25em] mb-[0.5em] w-[1.421em] -rotate-45 border-b-4 border-stone-500"
             style={{ fontSize: '2em' }}
           ></div>
         </div>
         <p
-          className="uppercase tracking-widest text-stone-400"
+          className="uppercase tracking-widest text-stone-100"
           style={{ fontSize: '3.5em', lineHeight: 1.2, marginLeft: '-0.1em' }}
         >
-          Open source
+          Open source React
           <br />
           clone
         </p>
